55cf40c10acdb1fcd7050640ad7014985dd7ea83
simeptkone <60314939+simeptkone@users.noreply.github.com>
2025-04-18 15:41:56 +0000

    today code

---
+++ apr1820205/ex01.py
@@ -0,0 +1,28 @@
+a = input("Enter A:")
+b = input("Enter B:")
+c = int(a) + int(b)
+print(a,b,c)
+
+# if (a>b):
+#     print("A is Big")
+#     print("B is Small")
+#     print("A and B are not equal")
+# else:
+#     print("B is Big")
+#     print("A is Small")
+#     print("A and B are not equal")
+
+# if(a==b):
+#     print("Both A and B are Equal")
+# elif(a>b):
+#     print("A is Big")
+# elif(a<b):
+#     print("B is Big")
+
+if(a==b):
+    print("A and B are Equal")
+else:
+    if(a>b):
+        print("A is Big")
+    else:
+        print("B is Big")
